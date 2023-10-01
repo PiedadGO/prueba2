05c1346aa158473e0222c8f5464f1bc1af78ea67
PiedadGO <piedad.pgo@gmail.com>
2023-10-01 18:33:31 +0200

    subida

---
+++ ej1.php
@@ -0,0 +1,16 @@
+<?php
+
+/*Muestra tres frases, cada una en un párrafo, utilizando las tres posibilidades que existen de mostrar contenido.
+    Tras ello introduce dos comentarios, uno de bloque y otro de línea.
+ */
+
+echo "<p> Esta es la frase 1 a mostrar en la página web. </p>"
+?>
+<p> Esta es la frase 2 a mostrar en la página web. </p>
+<?php
+print("<p>Esta es la frase 3 a mostrar en la página web.</p>");
+
+/*Esto es un comentario
+de bloque*/
+
+// Esto es un comentario de una línea
